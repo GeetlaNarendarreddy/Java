15db5a43e209d4b32159e42369e21ae295da8d11
GeetlaNarendarreddy <58568170+GeetlaNarendarreddy@users.noreply.github.com>
2021-10-24 10:28:53 +0530

    java commit

---
+++ Automation/src/selenium/Test.java
@@ -0,0 +1,32 @@
+package selenium;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 System.setProperty("webdriver.chrome.driver", "D:\\Narendar\\Projects\\Selenium\\chromedriver_win32\\chromedriver.exe");  
+		    WebDriver CD=new ChromeDriver();  
+		    CD.get("https://www.bewakoof.com/");
+		    String S=CD.getTitle();
+		    System.out.println(S);
+		  /* String S1=CD.getPageSource();
+		   System.out.println(S1);*/
+		   String S2=CD.getCurrentUrl();
+		   System.out.println(S2);
+		 //  CD.quit();
+		   CD.manage().window().maximize();
+		  // String S3=CD.manage().window().();
+		   
+		// CD.close();
+		 
+		   
+		   
+		    
+		      
+
+	}
+
+}
